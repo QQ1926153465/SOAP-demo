6e94465b93f54ddef729aa1d6ae80c257ce7a135
吴朝文 <1926153465@qq.com>
2023-02-16 15:39:24 +0800

    1

--- src/main/java/com/example/soapdemo/SoapDemoApplication.java
+++ src/main/java/com/example/soapdemo/SoapDemoApplication.java
@@ -2,9 +2,6 @@ package com.example.soapdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 
